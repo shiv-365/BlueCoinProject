f11c690711e0cf64d326d66ce0a13a96054221fd
Eric Foster <ericf@zipcoders-MacBook-Pro-3.local>
2018-07-16 14:10:28 -0400

    block controller

--- src/main/java/controllers/BlockController.java
+++ src/main/java/controllers/BlockController.java
@@ -3,6 +3,8 @@ package controllers;
 import block.Block;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import repository.BlockRepository;
 
@@ -10,18 +12,34 @@ import repository.BlockRepository;
 public class BlockController {
 
     @Autowired
-    BlockRepository blockRepository;
+    private BlockRepository blockRepository;
 
     //CREATE
     @RequestMapping(value = "blocks", method = RequestMethod.POST)
-    public Block create(@RequestBody Block block){
-        return blockRepository.saveAndFlush(block);
+    public ResponseEntity<Block> create(@RequestBody Block block){
+        return new ResponseEntity<Block>(blockRepository.saveAndFlush(block),HttpStatus.CREATED);
     }
 
     //READ
-    @RequestMapping(value = "blocks/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "users/{id}", method = RequestMethod.GET)
     public Block get(@PathVariable Long id){
         return blockRepository.findOne(id);
+    }
+
+    //UPDATE
+    @RequestMapping(value = "users/{id}", method = RequestMethod.PUT)
+    public Block update(@PathVariable Long id, @RequestBody Block block){
+        Block blockToUpdate = blockRepository.findOne(id);
+        BeanUtils.copyProperties(block, blockToUpdate);
+        return blockRepository.saveAndFlush(blockToUpdate);
+    }
+
+    //DELETE
+    @RequestMapping(value = "users/{id}", method = RequestMethod.DELETE)
+    public Block delete(@PathVariable Long id){
+        Block blockToDelete = blockRepository.findOne(id);
+        blockRepository.delete(blockToDelete);
+        return blockToDelete;
     }
 
 }
