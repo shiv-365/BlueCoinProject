01ebf7d1c5e03fc6a9559a0d8a5bdc097bde420f
Arin Turpin <ArinT@FedEx867.localdomain>
2018-07-16 14:37:35 -0400

    commit

--- src/test/java/redCoin/chainTest.java
+++ src/test/java/redCoin/chainTest.java
@@ -3,8 +3,12 @@ package redCoin;
 import block.Block;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class chainTest {
     Block block = new Block(0);
+    List<Block> blockchain = new ArrayList<Block>();
 
     @Test
     public void testAddBlock(){
