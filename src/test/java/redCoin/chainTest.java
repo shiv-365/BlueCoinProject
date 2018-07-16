f73daf86353487f4266fdb12274e55c55eb0182e
Arin Turpin <ArinT@FedEx867.localdomain>
2018-07-16 13:57:36 -0400

    testAndClass

---
+++ src/test/java/redCoin/chainTest.java
@@ -0,0 +1,14 @@
+package redCoin;
+
+import block.Block;
+import org.junit.Test;
+
+public class chainTest {
+    Block block = new Block(0);
+
+    @Test
+    public void testAddBlock(){
+
+    }
+
+}
