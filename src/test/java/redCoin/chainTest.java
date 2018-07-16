5cb2250d52baa121c611022ad989b4c1d31fa2f4
Arin Turpin <ArinT@FedEx867.localdomain>
2018-07-16 16:58:41 -0400

    ...

--- src/test/java/redCoin/chainTest.java
+++ src/test/java/redCoin/chainTest.java
@@ -2,6 +2,7 @@ package redCoin;
 
 import block.Block;
 import chain.Chain;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -10,11 +11,17 @@ import java.util.List;
 public class chainTest {
     Block block = new Block(0);
     Chain chain = new Chain();
+    //Miner miner = new Miner();
+
+    ArrayList<Block> blockchain = new ArrayList<Block>();
 
     @Test
-    public void testAddBlock(){
+    public void testAdd_Block_To_Blockchain(){
+
+        int x = blockchain.size();
 
 
+        Assert.assertTrue(x == 0);
     }
 
 }
