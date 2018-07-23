609acaa51ae35a96cdc0dff590b9da3a6a139574
Eric Foster <ericf@zipcoders-MacBook-Pro-3.local>
2018-07-23 11:53:38 -0400

    created basic control apis

--- ZipCoinRedJavaServer/src/main/java/com/zipcoin/controller/TransactionController.java
+++ ZipCoinRedJavaServer/src/main/java/com/zipcoin/controller/TransactionController.java
@@ -1,4 +1,38 @@
 package com.zipcoin.controller;
 
+import com.zipcoin.model.Transaction;
+import com.zipcoin.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/")
+@CrossOrigin(origins = {"http://localhost:8100"})
 public class TransactionController {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    //CREATE
+    @RequestMapping(value = "transactions", method = RequestMethod.POST)
+    public Transaction create(@RequestBody Transaction transaction){
+        return transactionRepository.saveAndFlush(transaction);
+    }
+
+    //READ-ALL
+    @RequestMapping(value = "transactions", method = RequestMethod.GET)
+    public List<Transaction> get(){
+        return transactionRepository.findAll();
+    }
+
+    //DELETE
+    @RequestMapping(value = "transactions/{id}", method = RequestMethod.DELETE)
+    public Transaction delete(@PathVariable Long id){
+        Transaction transactionToDelete = transactionRepository.findOne(id);
+        transactionRepository.delete(transactionToDelete);
+        return transactionToDelete;
+    }
+
 }
--- ZipCoinRedJavaServer/src/main/java/com/zipcoin/controller/WalletController.java
+++ ZipCoinRedJavaServer/src/main/java/com/zipcoin/controller/WalletController.java
@@ -1,4 +1,36 @@
 package com.zipcoin.controller;
 
+import com.zipcoin.model.Wallet;
+import com.zipcoin.repository.WalletRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/")
+@CrossOrigin(origins = {"http://localhost:8100"})
 public class WalletController {
+
+    @Autowired
+    private WalletRepository walletRepository;
+
+    //CREATE
+    @RequestMapping(value = "wallets", method = RequestMethod.POST)
+    public Wallet create(@RequestBody Wallet wallet){
+        return walletRepository.saveAndFlush(wallet);
+    }
+
+    //READ
+    @RequestMapping(value = "wallets/{id}", method = RequestMethod.GET)
+    public Wallet get(@PathVariable Long id){
+        return walletRepository.findOne(id);
+    }
+
+    //DELETE
+    @RequestMapping(value = "wallets/{id}", method = RequestMethod.DELETE)
+    public Wallet delete(@PathVariable Long id){
+        Wallet walletToDelete = walletRepository.findOne(id);
+        walletRepository.delete(walletToDelete);
+        return walletToDelete;
+    }
+
 }
