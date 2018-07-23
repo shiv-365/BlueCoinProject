20b46e4db8c2fa64927f0babe68107d5f8568092
Eric Foster <ericf@zipcoders-MacBook-Pro-3.local>
2018-07-23 12:13:36 -0400

    4 new apis for wallet and transaction

--- ZipCoinRedJavaServer/src/main/java/com/zipcoin/controller/TransactionController.java
+++ ZipCoinRedJavaServer/src/main/java/com/zipcoin/controller/TransactionController.java
@@ -15,19 +15,25 @@ public class TransactionController {
     @Autowired
     private TransactionRepository transactionRepository;
 
-    //CREATE
+    //CREATE A TRANSACTION
     @RequestMapping(value = "transactions", method = RequestMethod.POST)
     public Transaction create(@RequestBody Transaction transaction){
         return transactionRepository.saveAndFlush(transaction);
     }
 
-    //READ-ALL
+    //READ-ALL TRANSACTIONS
     @RequestMapping(value = "transactions", method = RequestMethod.GET)
     public List<Transaction> get(){
         return transactionRepository.findAll();
     }
 
-    //DELETE
+    //READ TRANSACTIONS TO OR FROM A PUBLIC KEY
+    @RequestMapping(value = "transactions{publicKey}", method = RequestMethod.GET)
+    public List<Transaction> get(@PathVariable String publicKey){
+        return transactionRepository.findAllTransactionsWithPubKey(publicKey);
+    }
+
+    //DELETE A TRANSACTION
     @RequestMapping(value = "transactions/{id}", method = RequestMethod.DELETE)
     public Transaction delete(@PathVariable Long id){
         Transaction transactionToDelete = transactionRepository.findOne(id);
--- ZipCoinRedJavaServer/src/main/java/com/zipcoin/controller/WalletController.java
+++ ZipCoinRedJavaServer/src/main/java/com/zipcoin/controller/WalletController.java
@@ -5,6 +5,8 @@ import com.zipcoin.repository.WalletRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/")
 @CrossOrigin(origins = {"http://localhost:8100"})
@@ -13,19 +15,25 @@ public class WalletController {
     @Autowired
     private WalletRepository walletRepository;
 
-    //CREATE
+    //CREATE A WALLET
     @RequestMapping(value = "wallets", method = RequestMethod.POST)
     public Wallet create(@RequestBody Wallet wallet){
         return walletRepository.saveAndFlush(wallet);
     }
 
-    //READ
-    @RequestMapping(value = "wallets/{id}", method = RequestMethod.GET)
-    public Wallet get(@PathVariable Long id){
-        return walletRepository.findOne(id);
+    //READ A WALLET FROM PUBLIC KEY
+    @RequestMapping(value = "wallets/{publicKey}", method = RequestMethod.GET)
+    public Wallet get(@PathVariable String publicKey){
+        return walletRepository.findWalletFromPubKey(publicKey);
     }
 
-    //DELETE
+    //READ PUBLIC KEYS FROM ALL WALLETS
+    @RequestMapping(value = "wallets/publicKeys", method = RequestMethod.GET)
+    public List<String> get(){
+        return walletRepository.findAllPublicKeys();
+    }
+
+    //DELETE A WALLET
     @RequestMapping(value = "wallets/{id}", method = RequestMethod.DELETE)
     public Wallet delete(@PathVariable Long id){
         Wallet walletToDelete = walletRepository.findOne(id);
