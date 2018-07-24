884b39c4632d5c51315f6384a65dbe7759fc9aff
Arin Turpin <ArinT@FedEx867.localdomain>
2018-07-24 17:54:00 -0400

    Get all blocks

--- ZipCoinRedJavaServer/src/main/java/com/zipcoin/controller/BlockController.java
+++ ZipCoinRedJavaServer/src/main/java/com/zipcoin/controller/BlockController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.zipcoin.repository.BlockRepository;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/")
 @CrossOrigin(origins = {"http://localhost:8100"})
@@ -22,9 +24,16 @@ public class BlockController {
     }
 
     //READ
+    //Gets a single Block by ID
     @RequestMapping(value = "blocks/{id}", method = RequestMethod.GET)
     public Block get(@PathVariable Long id){
         return blockRepository.findOne(id);
+    }
+
+    //Gets all Blocks
+    @RequestMapping(value = "blocks", method = RequestMethod.GET)
+    public List<Block> get(){
+        return blockRepository.findAll();
     }
 
     //UPDATE
