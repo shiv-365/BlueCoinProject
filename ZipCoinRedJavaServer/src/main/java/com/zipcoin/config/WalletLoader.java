fac06a3d0a744ee0ba6f09a54a0f70532b27972f
Navya Sanal <navyas@MacBook-Pro-5.local>
2018-08-02 08:03:04 -0400

    wallet server commit

--- ZipCoinRedJavaServer/src/main/java/com/zipcoin/config/WalletLoader.java
+++ ZipCoinRedJavaServer/src/main/java/com/zipcoin/config/WalletLoader.java
@@ -1,5 +1,8 @@
 package com.zipcoin.config;
 
+import com.zipcoin.model.Wallet;
+import com.zipcoin.repository.WalletRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -7,8 +10,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class WalletLoader implements ApplicationRunner {
 
+    private WalletRepository walletRepository;
+
+    @Autowired
+    public WalletLoader(WalletRepository walletRepository)
+    {
+        this.walletRepository = walletRepository;
+    }
     @Override
     public void run(ApplicationArguments applicationArguments) throws Exception {
+        Wallet[] wallets = new Wallet[]
+                {
+                        new Wallet("ANN","34",80),
+                        new Wallet("sen","3564",8)
+
+
+                };
+        for(Wallet wallet :wallets)
+        {
+            this.walletRepository.save(wallet);
+        }
+
 
     }
 }
+
