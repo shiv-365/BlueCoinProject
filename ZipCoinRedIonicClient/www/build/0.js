77076eb64f60b663e9338e26207d1639d9987b30
Eric Foster <ericf@zipcoders-MacBook-Pro-3.local>
2018-08-06 15:10:56 -0400

    fixed merge conflicts with dev

--- ZipCoinRedIonicClient/www/build/0.js
+++ ZipCoinRedIonicClient/www/build/0.js
@@ -8,12 +8,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "WalletPageModule", function() { return WalletPageModule; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(28);
-<<<<<<< HEAD
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__wallet__ = __webpack_require__(103);
-=======
-
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__wallet__ = __webpack_require__(104);
->>>>>>> 4a69e031c9b8598a029ffbcbebf3ef5a408f8d83
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__providers_transaction_service__ = __webpack_require__(50);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
